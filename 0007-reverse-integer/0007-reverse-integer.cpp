d0eb4ebefe48590ca49cef188c03e28649dc9ac4
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2022-10-28 12:35:51 +0530

    Time: 4 ms (46.70%), Space: 6 MB (29.43%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        bool neg = x<0;
+        int x1=abs(x);
+        int ans=0;
+        while(x1>0){
+            if(ans> INT_MAX/10 || ans<INT_MIN/10) return 0;
+            ans = ans*10 + x1%10;
+            x1 = x1/10;
+        }
+        if(neg) return -1*ans;
+        return ans;
+    }
+};
