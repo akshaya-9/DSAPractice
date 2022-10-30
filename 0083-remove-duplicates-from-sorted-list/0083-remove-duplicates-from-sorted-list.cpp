4f9bf420837e48ff5a20a19450479dd8817f3ec3
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2022-10-30 23:10:08 +0530

    Update 0083-remove-duplicates-from-sorted-list.cpp

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -13,8 +13,7 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode* ans =head;
         while(ans!=NULL && ans->next!=NULL){
-            if(ans->val == ans->next->val)
-                ans->next = ans->next->next;
+            if(ans->val == ans->next->val) ans->next = ans->next->next;
             else ans = ans->next;
         }
         return head;
