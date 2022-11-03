6ffd6dd6420526fa78449104b6a359d0bffc433b
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2022-11-03 23:14:03 +0530

    Update 0217-contains-duplicate.cpp

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,9 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         map<int,int> m;
         for(int i=0;i<nums.size();i++){
-            if(m.find(nums[i])!=m.end()){
-                return true;
-            }
+            if(m.find(nums[i])!=m.end())  return true;
             m[nums[i]]++;
         }
         return false;
