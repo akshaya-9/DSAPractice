2999aaa07cc698b277d13c4fecc9272bf767072c
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2024-09-29 01:20:36 +0530

    Time: 6 ms (49.83%) | Memory: 15.6 MB (98.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,9 +4,10 @@ public:
         int m=prices[0], ans=0, sum=0;
         for(int i=1;i<prices.size();i++){
             m = min(m, prices[i]);
-            if(prices[i] > m) { sum = prices[i]-m;
-            ans = ans + sum;
-            m = prices[i];
+            if(prices[i] > m) { 
+                sum = prices[i]-m;
+                ans = ans + sum;
+                m = prices[i];
             }
         }
         return ans;
