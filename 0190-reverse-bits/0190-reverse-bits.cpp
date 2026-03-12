e15157875455eee00fe2633688b47b33214e542e
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-03-12 21:44:22 +0530

    Time: 4 ms (29.34%), Space: 8.3 MB (46.51%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,9 +3,9 @@ public:
     int reverseBits(int n) {
         int result =0;
         for(int i=1;i<=32;i++) {
-            result = result<<1;
-            result = result  | (n&1);
-            n = n>>1;
+            result = result<<1; // left shift to make space for LSB
+            result = result  | (n&1); // add LSB to result
+            n = n>>1; // remove LSB from n
         }
         return result;
     }
