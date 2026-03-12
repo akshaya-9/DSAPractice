fceb0a0b0428a1a9336e3646c8051fd9240cbf26
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-03-12 21:46:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.51%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int reverseBits(int n) {
         int result =0;
-        for(int i=1;i<=32;i++) {
+        for(int i=0;i<32;i++) {
             result = result<<1; // left shift to make space for LSB
             result = result  | (n&1); // add LSB to result
             n = n>>1; // remove LSB from n
