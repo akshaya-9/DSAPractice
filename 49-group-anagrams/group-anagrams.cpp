487c7d8ea51815975fcce45d9a137b5f977d62ac
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-03-13 12:43:32 +0530

    Time: 16 ms (59.26%) | Memory: 24.9 MB (78.31%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<string, vector<string>> m;
+        for(int i=0;i<strs.size();i++) {
+            string sorted = strs[i];
+            sort(sorted.begin(), sorted.end());
+            if(m.find(sorted) != m.end()) {
+                m[sorted].push_back(strs[i]);
+            }
+            else {
+                m[sorted].push_back(strs[i]);
+            }
+        }
+        for(auto &p:m) {
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
+};
