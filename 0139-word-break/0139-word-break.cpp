09069d43c6d2e55932c3b7d20e17f3666a621c2c
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-06-02 16:28:49 +0530

    Time: 15 ms (25.65%), Space: 18.8 MB (12.32%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,19 +1,20 @@
 class Solution {
-public:
-    bool check_word(int l, int r, string s, unordered_set<string>& words,  vector<vector<int>>& dp) {
-        if(r>s.length()) return 0;
-        if(dp[l][r]!=-1) return dp[l][r];
-        if(words.find(s.substr(l, r-l)) != words.end()) {
-            if(r==s.length() || check_word(r,r+1,s,words, dp)) return dp[l][r] = 1;
+private:
+    bool func(string s, unordered_set<string>& dict, int start, vector<int>& dp) {
+        if(start==s.length()) return 1;
+        if (dp[start]!= -1) return dp[start];
+        for(int end=start+1;end<=s.length();end++){
+            string word =s.substr(start,end-start);
+            if((dict.find(word)!=dict.end()) && func(s,dict,end,dp)) {
+                return dp[start] = 1;
+            }
         }
-        return dp[l][r] = check_word(l,r+1,s,words, dp);
+        return dp[start]=0;
     }
-    
+public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int l=0,r=0;
-        int n =s.length();
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return check_word(l,r+1,s, words, dp);    
+        unordered_set<string> dict(wordDict.begin(),wordDict.end());
+        vector<int> dp(s.length(),-1);
+        return func(s,dict,0,dp);
     }
 };
