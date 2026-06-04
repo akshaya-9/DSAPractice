9539187af813dc6e23ff1afedb4d0b20f0860f1c
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-06-04 12:01:42 +0530

    Time: 7 ms (67.02%) | Memory: 15.3 MB (51.99%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private:
-    int ways(vector<int>& nums, int k,int idx) {
+    int ways(vector<int>& nums, int k,int idx,vector<vector<int>>& dp) {
         if(idx==0) {
            if(k==0 && nums[0]==0) return 2;
            if(k==0 || nums[0]==k) return 1;
            return 0; 
         }
+        if(dp[idx][k]!=-1) return dp[idx][k];
         int pick= 0;
         if(nums[idx]<=k){
-            pick = ways(nums,k-nums[idx],idx-1);
+            pick = ways(nums,k-nums[idx],idx-1,dp);
         }
-        int not_pick = ways(nums, k, idx-1);
-        return pick+not_pick;
+        int not_pick = ways(nums, k, idx-1,dp);
+        return dp[idx][k]=pick+not_pick;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -22,6 +23,8 @@ public:
         int n=nums.size();
         if(sum-target<0) return 0;
         if((sum-target)%2==1) return 0;
-        return ways(nums, (sum-target)/2, n-1);
+        int req =(sum-target)/2;
+        vector<vector<int>> dp(n,vector<int>(req+1,-1));
+        return ways(nums, req, n-1,dp);
     }
 };
