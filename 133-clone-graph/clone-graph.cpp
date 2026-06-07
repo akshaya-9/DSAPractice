1300299fbf95a8c6f27984c3ee7bb90b4a478a42
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-06-07 23:29:11 +0530

    Time: 4 ms (34.04%) | Memory: 12.3 MB (26.00%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    Node* clone(Node* node, unordered_map<Node*,Node*>& mp){
+        Node* newnode = new Node(node->val);
+        mp[node] =newnode;
+        for(auto neigh: node->neighbors) {
+            if(mp.find(neigh)==mp.end()) {
+                newnode->neighbors.push_back(clone(neigh,mp));
+            } else{
+                newnode->neighbors.push_back(mp[neigh]);
+            }
+        }
+        return newnode;
+    }
+
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==NULL) return node;
+        unordered_map<Node*,Node*> mp;
+        return clone(node,mp);
+    }
+};
