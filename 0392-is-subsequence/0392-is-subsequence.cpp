43be6ad44ebc7a2eb6a4338a6c50396624bee22b
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-06-18 22:49:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.35%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int left=0,right=0;
+        while(left<s.length() && right<t.length()){
+            if(s[left]==t[right]){
+                left++;
+                right++;
+            }
+            else {
+                right++;
+            }
+        }
+        return left==s.length()? true: false;
+    }
+};
