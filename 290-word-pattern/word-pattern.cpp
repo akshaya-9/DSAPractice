1206167d7718d3576cbd78c8de1f46f0515bbe9b
Lalitha Akshaya <55704423+akshaya-9@users.noreply.github.com>
2026-09-16 23:43:05 +0530

    Time: 3 ms (3.82%) | Memory: 9.2 MB (11.35%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char,string> m;
+        set<string> used;
+        int j=0;
+        for(int i=0;i<pattern.length();i++){
+            stack<char> st;
+            string word="";
+            if(j>=s.length()){
+                return false;
+            }
+            while(j<s.length() && s[j]!=' '){
+                st.push(s[j]);
+                j++;
+            }
+            j = j+1;
+            while(!st.empty()) {
+                word += st.top();
+                st.pop();
+            }
+            reverse(word.begin(),word.end());
+            cout<<"word "<<word<<endl;
+            if(m.find(pattern[i])==m.end()){
+                if(used.find(word)!=used.end()){
+                    return false;
+                }
+                m[pattern[i]] = word;
+                used.insert(word);
+            } else if(m[pattern[i]]!=word){
+                return false;
+            }
+        }
+        if(j<s.length()) return false;
+        return true;
+    }
+};
